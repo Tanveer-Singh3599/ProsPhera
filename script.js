d980a86e99475cecafce114f443ebd1fe255d9d6
Tanveer-Singh3599 <tanveersingh3599@gmail.com>
2025-03-30 06:17:49 +0530

    Major update

---
+++ script.js
@@ -0,0 +1,28 @@
+// let username= "Gna";
+// let pass= "1234";
+
+// function login(){
+//     let a= document.getElementById("user").value;
+//     let b= document.getElementById("password").value;
+
+//     if ((username== a)&& (pass=="1234")){
+//         window.location.href = "./Dashboard.html";
+
+//     }else {
+//             document.getElementById("message").innerText = "Invalid username or password!";
+//         }    
+//     }
+
+let username = "Gna";
+let pass = 1234;
+
+function login() {
+    let a = document.getElementById("user");
+    let b = document.getElementById("password").value;
+
+    if (username === a && pass === b) {
+        window.open("Dashboard.html");
+    } else {
+        alert("Invalid username or password!")
+    }
+}
